83062f0c93ed3c277c2d78b5292fa8d7e1ae4726
Joseph Shambrook <jshambrook@and.digital>
2018-10-10 12:05:04 +0100

    First version of form demo

--- src/js/rms.js
+++ src/js/rms.js
@@ -1,5 +1,40 @@
-console.log('rms');
+(function() {
+  $(function() {
 
-const fun = () => console.log('hey');
+    /*
+     * Handle Legal Entity Dropdown
+     */
 
-fun();
+    // store all relevant forms in a variable
+    const forms = $('.js-legal-entity-form');
+
+    $('#legal-entities-picker').on('change', function() {
+      // get the value of the option
+      const val = $(this).val();
+
+      // get the form related to the chosen option
+      const form = forms.filter(`[data-entity-type="${val}"]`);
+
+      // check if the form was found and is not already displayed
+      if (form.length && !form.hasClass('d-block')) {
+        // hide the form which is currently displayed
+        $('.js-legal-entity-form.d-block').removeClass('d-block').addClass('d-none');
+
+        // show the form which was chosen to show
+        form.removeClass('d-none').addClass('d-block');
+      }
+    });
+
+    /*
+     * Handle modal address submission
+     */
+
+    const findAddressModal = $('#find-address');
+
+    findAddressModal.on('click', '.js-find-address-submit', function() {
+      findAddressModal.modal('hide');
+      $('.js-address-details').removeClass('d-none');
+    });
+
+  });
+}());
