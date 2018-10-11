b619db2caac00c0061ffedc160be693682d42c43
Joseph Shambrook <jshambrook@and.digital>
2018-10-11 09:46:58 +0100

    Created way of concatenating and passing JS through babel, to be one file rather than many

--- dist/js/rms-transpiled.js
+++ dist/js/rms-transpiled.js
@@ -1,47 +1,39 @@
-(function() {
-  $(function() {
+"use strict";
 
+(function () {
+  $(function () {
     /*
      * Handle Legal Entity Dropdown
      */
-
     // store all relevant forms in a variable
-    const forms = $('.js-legal-entity-form');
-
-    $('#legal-entities-picker').on('change', function() {
+    var forms = $('.js-legal-entity-form');
+    $('#legal-entities-picker').on('change', function () {
       // get the value of the option
-      const val = $(this).val();
+      var val = $(this).val(); // get the form related to the chosen option
 
-      // get the form related to the chosen option
-      const form = forms.filter(`[data-entity-type="${val}"]`);
+      var form = forms.filter("[data-entity-type=\"".concat(val, "\"]")); // check if the form was found and is not already displayed
 
-      // check if the form was found and is not already displayed
       if (form.length && !form.hasClass('d-block')) {
         // hide the form which is currently displayed
-        $('.js-legal-entity-form.d-block').removeClass('d-block').addClass('d-none');
+        $('.js-legal-entity-form.d-block').removeClass('d-block').addClass('d-none'); // show the form which was chosen to show
 
-        // show the form which was chosen to show
         form.removeClass('d-none').addClass('d-block');
       }
     });
-
     /*
      * Handle modal address submission
      */
 
-    const findAddressModal = $('#js-modal-find-address');
-
-    findAddressModal.on('click', '.js-find-address-submit', function() {
+    var findAddressModal = $('#js-modal-find-address');
+    findAddressModal.on('click', '.js-find-address-submit', function () {
       findAddressModal.modal('hide');
       $('.js-business-details-form').removeClass('d-none');
     });
-
     /*
      * Handle modal for custom VAT number
      */
 
-    const customVatModal = $('#js-modal-custom-vat');
-
+    var customVatModal = $('#js-modal-custom-vat');
     /*
      * Initialize select2 plugin for operating countries tag picker
      */
@@ -50,4 +42,4 @@
       width: '100%'
     });
   });
-}());
+})();
